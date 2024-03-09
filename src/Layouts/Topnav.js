ce7fd7b3528c1327075cebbd6e6b81787ec3884e
Vijay Kale <105584316+vijayK006@users.noreply.github.com>
2024-03-09 04:10:44 +0000

    yotube and icons

--- src/Layouts/Topnav.js
+++ src/Layouts/Topnav.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import logo from '../Assets/img/YouTube.png';
+import { IoSearchOutline } from "react-icons/io5";
+import { VscMenu } from "react-icons/vsc";
+
 
 const Topnav = () => {
   return (
     <>
 <nav>
 <div className='logo'>
-
+<VscMenu />
+<img src={logo} alt='youtube-logo' />
 </div>
 
 <div className='search-bar'>
 <form>
 <input type='text' placeholder='Search' name='search-v' id='search-v'/>
-<button type='button'></button>
+<button type='button'><IoSearchOutline /></button>
 </form> 
 </div>
 
